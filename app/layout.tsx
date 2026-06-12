8371679afc53f62d1ab1d33f2a87b20ec92fa280
Ben Hatch <ben.m.hatch@gmail.com>
2026-06-12 10:51:49 -0400

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 			<body>
 				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
 				<ClientLayout>{children}</ClientLayout>
+				<Analytics />
 			</body>
 		</html>
 	);
